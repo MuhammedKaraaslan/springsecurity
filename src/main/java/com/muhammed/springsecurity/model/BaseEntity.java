f21d8a5b8f7b3ab58c8175de2638a2ce7a89020d
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2024-03-27 18:26:42 +0300

    Add BaseEntity, Permission enum, and Role enum

---
+++ src/main/java/com/muhammed/springsecurity/model/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.muhammed.springsecurity.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
+
+
+    @CreatedBy
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+
+}
