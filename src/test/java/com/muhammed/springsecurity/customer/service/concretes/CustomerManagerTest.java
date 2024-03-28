c82e8314bede9d4c644bef3b284f808c8572159b
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2024-03-29 00:19:42 +0300

    Add new test methods to CustomerManagerTest

--- src/test/java/com/muhammed/springsecurity/customer/service/concretes/CustomerManagerTest.java
+++ src/test/java/com/muhammed/springsecurity/customer/service/concretes/CustomerManagerTest.java
@@ -3,19 +3,27 @@ package com.muhammed.springsecurity.customer.service.concretes;
 import com.muhammed.springsecurity.abstracts.AbstractServiceTest;
 import com.muhammed.springsecurity.customer.dataAccess.abstracts.CustomerDao;
 import com.muhammed.springsecurity.customer.model.entities.Customer;
+import com.muhammed.springsecurity.customer.model.requests.CustomerLoginRequest;
 import com.muhammed.springsecurity.customer.model.requests.CustomerRegistrationRequest;
+import com.muhammed.springsecurity.customer.model.responses.CustomerLoginResponse;
 import com.muhammed.springsecurity.customer.model.responses.CustomerRegistrationResponse;
+import com.muhammed.springsecurity.exceptions.BusinessException;
 import com.muhammed.springsecurity.security.dataAccess.abstracts.TokenDao;
+import com.muhammed.springsecurity.security.model.entities.Token;
 import com.muhammed.springsecurity.security.service.abstracts.JwtService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 class CustomerManagerTest extends AbstractServiceTest {
 
@@ -37,7 +45,6 @@ class CustomerManagerTest extends AbstractServiceTest {
     @InjectMocks
     private CustomerManager underTest;
 
-    // add test for register method
     @Test
     void Given_ValidCustomerRegistrationRequest_When_RegisterIsCalled_Then_ReturnCustomerRegistrationResponse() {
         // Given
@@ -54,20 +61,107 @@ class CustomerManagerTest extends AbstractServiceTest {
 
         CustomerRegistrationResponse expected = new CustomerRegistrationResponse(jwtToken, refreshToken);
 
-        when(passwordEncoder.encode(customer.getPassword())).thenReturn("encodedPassword");
-        customer.setPassword("encodedPassword");
+        when(passwordEncoder.encode(customer.getPassword())).thenReturn(customer.getPassword());
         when(jwtService.generateToken(customer)).thenReturn(jwtToken);
         when(jwtService.generateRefreshToken(customer)).thenReturn(refreshToken);
         when(customerDao.save(customer)).thenReturn(customer);
         when(tokenDao.save(any())).thenReturn(null);
-
-        System.out.println("sadsadsa");
 
         // When
         CustomerRegistrationResponse actual = underTest.register(customerRegistrationRequest);
 
         // Then
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void Given_ValidCustomerLoginRequest_When_LoginIsCalled_Then_ReturnCustomerLoginResponse() {
+        // Given
+        String jwtToken = "jwtToken";
+        String refreshToken = "refreshToken";
+        String email = "test@email.com";
+        String password = "Password1.";
+
+        CustomerLoginRequest customerLoginRequest =
+                new CustomerLoginRequest(email, password);
+
+        Customer customer = new Customer(
+                email,
+                password,
+                "dummyFirstname",
+                "dummyLastname");
+
+        CustomerLoginResponse expected = new CustomerLoginResponse(jwtToken, refreshToken);
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(customer, null);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(jwtService.generateToken(customer)).thenReturn(jwtToken);
+        when(jwtService.generateRefreshToken(customer)).thenReturn(refreshToken);
+        when(tokenDao.save(any())).thenReturn(null);
+
+        // When
+        CustomerLoginResponse actual = underTest.login(customerLoginRequest);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void Given_ValidCustomerLoginRequestAndValidTokens_When_LoginIsCalled_Then_ReturnCustomerLoginResponse() {
+        // Given
+        String jwtToken = "jwtToken";
+        String refreshToken = "refreshToken";
+        String email = "test@email.com";
+        String password = "Password1.";
+
+        CustomerLoginRequest customerLoginRequest =
+                new CustomerLoginRequest(email, password);
+
+        Customer customer = new Customer(
+                email,
+                password,
+                "dummyFirstname",
+                "dummyLastname");
+
+        Token token = new Token();
+        token.setUser(customer);
+
+        CustomerLoginResponse expected = new CustomerLoginResponse(jwtToken, refreshToken);
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(customer, null);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(jwtService.generateToken(customer)).thenReturn(jwtToken);
+        when(jwtService.generateRefreshToken(customer)).thenReturn(refreshToken);
+        when(tokenDao.findAllValidTokenByUser(anyInt())).thenReturn(List.of(token));
+        when(tokenDao.save(any())).thenReturn(null);
+
+        // When
+        CustomerLoginResponse actual = underTest.login(customerLoginRequest);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void Given_InvalidCustomerLoginRequest_When_LoginIsCalled_Then_ThrowBusinessException() {
+        // Given
+        CustomerLoginRequest invalidRequest =
+                new CustomerLoginRequest(
+                        "invalid@email.com",
+                        "WrongPassword");
+
+        when(authenticationManager.authenticate(any()))
+                .thenThrow(new BusinessException("Invalid credentials"));
+
+        // Then
+        assertThrows(BusinessException.class, () -> underTest.login(invalidRequest));
+
+        // Verify that authentication manager was called once
+        verify(authenticationManager, times(1)).authenticate(any());
     }
 
 }
