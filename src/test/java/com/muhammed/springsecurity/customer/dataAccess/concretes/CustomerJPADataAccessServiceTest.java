4599742f91b36ba1c2697ee888255ed817463d54
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2024-03-28 16:06:54 +0300

    Add CustomerJPADataAccessServiceTest

---
+++ src/test/java/com/muhammed/springsecurity/customer/dataAccess/concretes/CustomerJPADataAccessServiceTest.java
@@ -0,0 +1,74 @@
+package com.muhammed.springsecurity.customer.dataAccess.concretes;
+
+import com.muhammed.springsecurity.customer.dataAccess.abstracts.CustomerRepository;
+import com.muhammed.springsecurity.customer.model.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerJPADataAccessServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @InjectMocks
+    private CustomerJPADataAccessService underTest;
+
+    @BeforeEach
+    void setUp() {
+        reset(customerRepository);
+    }
+
+    @Test
+    void should_ReturnSavedCustomer_When_RepositorySavesCustomer() {
+        //Given
+        String email = "test@email.com";
+        Customer expected = new Customer(email, "Password1.", "dummyFirstname", "dummyLastname");
+
+        when(customerRepository.save(expected)).thenReturn(expected);
+
+        // Then
+        assertEquals(expected, underTest.save(expected));
+        verify(customerRepository, times(1)).save(expected);
+    }
+
+    @Test
+    void should_ReturnCustomer_When_CustomerExistsByEmail() {
+        //Given
+        String email = "test@email.com";
+        Customer expected = new Customer(email, "Password1.", "dummyFirstname", "dummyLastname");
+
+        when(customerRepository.findByEmail(email)).thenReturn(Optional.of(expected));
+
+        // When
+        Optional<Customer> foundCustomer = underTest.findByEmail(email);
+
+        // Then
+        assertTrue(foundCustomer.isPresent());
+        assertEquals(expected, foundCustomer.get());
+    }
+
+    @Test
+    void should_ReturnEmptyOptional_When_CustomerDoesNotExistByEmail() {
+        // Given
+        String email = "nonexistent@email.com";
+
+        when(customerRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        // When
+        Optional<Customer> foundCustomer = underTest.findByEmail(email);
+
+        // Then
+        assertTrue(foundCustomer.isEmpty());
+    }
+
+}
