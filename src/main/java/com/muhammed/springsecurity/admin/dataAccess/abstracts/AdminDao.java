9d75ed6f7c5ac759f1fccad18c9d229596401c07
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2024-03-30 13:35:53 +0300

    Add AdminDao and AdminJPADataAccessService

---
+++ src/main/java/com/muhammed/springsecurity/admin/dataAccess/abstracts/AdminDao.java
@@ -0,0 +1,13 @@
+package com.muhammed.springsecurity.admin.dataAccess.abstracts;
+
+import com.muhammed.springsecurity.admin.model.entities.Admin;
+
+import java.util.Optional;
+
+public interface AdminDao {
+
+    Admin save(Admin admin);
+
+    Optional<Admin> findByEmail(String email);
+
+}
---
+++ src/main/java/com/muhammed/springsecurity/admin/dataAccess/concretes/AdminJPADataAccessService.java
@@ -0,0 +1,27 @@
+package com.muhammed.springsecurity.admin.dataAccess.concretes;
+
+import com.muhammed.springsecurity.admin.dataAccess.abstracts.AdminDao;
+import com.muhammed.springsecurity.admin.dataAccess.abstracts.AdminRepository;
+import com.muhammed.springsecurity.admin.model.entities.Admin;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository("admin-jpa")
+@RequiredArgsConstructor
+public class AdminJPADataAccessService implements AdminDao {
+
+    private final AdminRepository adminRepository;
+
+
+    @Override
+    public Admin save(Admin admin) {
+        return this.adminRepository.save(admin);
+    }
+
+    @Override
+    public Optional<Admin> findByEmail(String email) {
+        return this.adminRepository.findByEmail(email);
+    }
+}
